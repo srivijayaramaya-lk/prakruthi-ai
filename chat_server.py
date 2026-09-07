d47265bbf85458a3f82b488ca422f2ebd4ca2690
srivijayaramaya-lk <prakruthiapp@gmail.com>
2026-09-07 20:05:48 +0530

    Update chat_server.py

--- chat_server.py
+++ chat_server.py
@@ -365,6 +365,7 @@ from fastapi.responses import FileResponse
 @app.get("/pk_features.js")
 def pk_features_js():
     return FileResponse("pk_features.js", media_type="application/javascript")
+    headers={"Cache-Control": "no-store, must-revalidate"})
 
 if __name__ == "__main__":
     import socket, uvicorn
